9b2261764794b3514aa15b8a8505334fb90998d1
Giovanni <jacket.gav@gmail.com>
2023-12-07 16:25:46 +0100

    fix: add constructors for unpacking responses

--- src/main/java/com/aerotrack/model/FlightPair.java
+++ src/main/java/com/aerotrack/model/FlightPair.java
@@ -2,10 +2,12 @@ package com.aerotrack.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @ToString
 public class FlightPair {
     private Flight outboundFlight;
