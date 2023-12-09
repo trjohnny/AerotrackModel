805d0f4571e5f2ed92ef125bd5ffc701d8976f2f
Giovanni <jacket.gav@gmail.com>
2023-12-09 16:50:22 +0100

    refactor: airport json file structure and naming

---
+++ src/main/java/com/aerotrack/model/entities/AirportsJsonFile.java
@@ -0,0 +1,17 @@
+package com.aerotrack.model.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+
+@Data
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+public class AirportsJsonFile {
+    private List<Airport> airportList;
+}
