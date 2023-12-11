56ca907b16b547f360874f27add703202e0b4fd0
Giovanni <jacket.gav@gmail.com>
2023-12-11 11:35:56 +0100

    feat: field validation for ScanQueryRequest

--- src/main/java/com/aerotrack/model/protocol/ScanQueryRequest.java
+++ src/main/java/com/aerotrack/model/protocol/ScanQueryRequest.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Data
@@ -19,4 +21,19 @@ public class ScanQueryRequest {
     private List<String> departureAirports;
     private List<String> destinationAirports;
     private Boolean returnToSameAirport;
+
+    public void validate() throws IllegalArgumentException {
+        LocalDate today = LocalDate.now();
+        LocalDate start = LocalDate.parse(availabilityStart, DateTimeFormatter.ISO_LOCAL_DATE);
+        LocalDate end = LocalDate.parse(availabilityEnd, DateTimeFormatter.ISO_LOCAL_DATE);
+        LocalDate oneYearFromNow = today.plusYears(1);
+
+        if (minDays < 0) throw new IllegalArgumentException("minDays must be greater or equal than 0");
+        if (minDays > maxDays) throw new IllegalArgumentException("minDays must not exceed maxDays");
+        if (maxDays >= 90) throw new IllegalArgumentException("maxDays must be less than 90");
+        if (start.isBefore(today)) throw new IllegalArgumentException("availabilityStart must be a future date");
+        if (start.isAfter(end)) throw new IllegalArgumentException("availabilityStart must be before availabilityEnd");
+        if (maxDays - minDays < end.toEpochDay() - start.toEpochDay()) throw new IllegalArgumentException("maxDays - minDays must be greater than or equal to availabilityEnd - availabilityStart");
+        if (end.isAfter(oneYearFromNow)) throw new IllegalArgumentException("availabilityEnd must be within one year from today");
+    }
 }
