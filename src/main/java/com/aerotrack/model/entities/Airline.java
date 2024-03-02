a976925204f2d22d3549f4f3cac98f865ea374e7
Giovanni <jacket.gav@gmail.com>
2024-03-02 09:56:43 +0100

    feat: airline enum

--- src/main/java/com/aerotrack/model/entities/Airline.java
+++ src/main/java/com/aerotrack/model/entities/Airline.java
@@ -1,11 +1,22 @@
 package com.aerotrack.model.entities;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Arrays;
 
 @AllArgsConstructor
+@Getter
 public enum Airline {
     RYANAIR("Ryanair"),
     WIZZAIR("Wizzair");
 
     private final String airlineName;
+
+    public static Airline fromName(String name) {
+        return Arrays.stream(Airline.values())
+                .filter(airline -> airline.getAirlineName().equalsIgnoreCase(name))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("No enum constant " + Airline.class.getCanonicalName() + "." + name));
+    }
 }
