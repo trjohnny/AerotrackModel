3a2ed39a29406a7c8c741fe3f3247a2e3315ee61
Giovanni <jacket.gav@gmail.com>
2024-03-10 17:59:32 +0100

    feat: custom equals and hashCode for airport

--- src/main/java/com/aerotrack/model/entities/Airport.java
+++ src/main/java/com/aerotrack/model/entities/Airport.java
@@ -3,15 +3,14 @@ package com.aerotrack.model.entities;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.Objects;
 
 @Data
 @NoArgsConstructor
 @Builder
-@EqualsAndHashCode
 @AllArgsConstructor
 public class Airport {
     private String airportCode;
@@ -19,4 +18,17 @@ public class Airport {
     private String countryCode;
     private List<String> connections;
     private String lastUpdatedDateTime;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Airport airport = (Airport) o;
+        return Objects.equals(airportCode, airport.airportCode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(airportCode);
+    }
 }
